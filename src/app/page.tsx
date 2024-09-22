2cc5eb5e95a8f0721ff83a9890ab9881b764303b
kauavitorrodrigues <kvrfz.prsnl@gmail.com>
2024-09-22 08:07:20 -0300

    add: implement root page redirect to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Logo } from "@/components/ui/Logo";
+import { redirect } from "next/navigation";
+
 export default function Page() {
 
+	redirect("/home")
+
 	return (
-		<div>...</div>
+		<div className="min-h-screen flex justify-center items-center">
+			<Logo size={80} />
+		</div>
 	)
 
 }
