fdb0e6085b195f2605d5e78abfde0a60b493dbad
kauavitorrodrigues <kvrfz.prsnl@gmail.com>
2024-09-22 08:22:13 -0300

    add: create ui general layout

---
+++ src/app/(ui)/home/page.tsx
@@ -0,0 +1,11 @@
+import { Logo } from "@/components/ui/Logo";
+
+export default function Page() {
+
+	return (
+		<div className="flex justify-center items-center">
+			<Logo size={80} />
+		</div>
+	)
+
+}
